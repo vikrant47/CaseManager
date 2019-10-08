85251bf0f2657b8a977265b4f49cdd1ed949007b
vikrant singh <vikrant.singh1990@yahoo.in>
2019-10-08 11:06:31 +0530

    Created workflow table

---
+++ www/plugins/demo/casemanager/updates/builder_table_create_demo_casemanager_workflows.php
@@ -0,0 +1,27 @@
+<?php namespace Demo\Casemanager\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateDemoCasemanagerWorkflows extends Migration
+{
+    public function up()
+    {
+        Schema::create('demo_casemanager_workflows', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->integer('active')->default(1);
+            $table->string('name');
+            $table->text('description');
+            $table->text('definition');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('demo_casemanager_workflows');
+    }
+}
