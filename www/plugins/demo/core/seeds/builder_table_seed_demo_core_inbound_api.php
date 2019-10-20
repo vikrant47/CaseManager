a53ba58872ddbd6330c05b064457f8e7b99038a7
vikrant singh <vikrant.singh1990@yahoo.in>
2019-10-20 14:20:33 +0530

    Added seeds

---
+++ www/plugins/demo/core/seeds/builder_table_seed_demo_core_inbound_api.php
@@ -0,0 +1,13 @@
+<?php
+namespace Demo\Core\Seeds;
+
+use Schema;
+use Seeder;
+
+class BuilderTableSeedDemoCoreInboundApi extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
--- www/plugins/demo/core/updates/builder_table_create_demo_core_inbound_api.php
+++ www/plugins/demo/core/updates/builder_table_create_demo_core_inbound_api.php
@@ -1,29 +1,29 @@
-<?php namespace Demo\Casemanager\Updates;
+<?php namespace Demo\Core\Updates;
 
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class BuilderTableCreateDemoCasemanagerWorkflowStates extends Migration
+class BuilderTableCreateDemoCoreInboundApi extends Migration
 {
     public function up()
     {
-        Schema::create('demo_casemanager_workflow_states', function($table)
-        {
+        Schema::create('demo_core_inbound_api', function ($table) {
             $table->engine = 'InnoDB';
             $table->increments('id')->unsigned();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
             $table->integer('created_by_id');
             $table->integer('updated_by_id');
+            $table->string('method');
+            $table->text('script');
             $table->string('name');
-            $table->text('description');
-            $table->integer('active');
-            $table->string('code');
+            $table->text('description')->nullable();
+            $table->text('url');
         });
     }
-    
+
     public function down()
     {
-        Schema::dropIfExists('demo_casemanager_workflow_states');
+        Schema::dropIfExists('demo_core_inbound_api');
     }
 }
