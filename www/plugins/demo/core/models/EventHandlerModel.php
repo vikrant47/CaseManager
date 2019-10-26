c217d65ce5e30b63efacef334420f36ef5cb2615
vikrant singh <vikrant.singh1990@yahoo.in>
2019-10-26 20:54:27 +0530

    Some ui modification done

--- www/plugins/demo/core/models/EventHandlerModel.php
+++ www/plugins/demo/core/models/EventHandlerModel.php
@@ -24,6 +24,12 @@ class EventHandlerModel extends Model
 
     public $attachAuditedBy = true;
 
+    public $belongsTo = [
+        'created_by' => [User::class, 'key' => 'created_by_id'],
+        'updated_by' => [User::class, 'key' => 'updated_by_id'],
+        'plugin' => [PluginModel::class, 'key' => 'plugin_id']
+    ];
+
     public function getModelOptions()
     {
         return PluginConnection::getAllModelAlias(true);
