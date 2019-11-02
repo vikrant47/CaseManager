a0818cb755560639e378b99a7e1b13fdda7e9b71
vikrant singh <vikrant.singh1990@yahoo.in>
2019-11-02 20:57:08 +0530

    Workflow Moved

--- www/plugins/demo/casemanager/models/CaseModel.php
+++ www/plugins/demo/casemanager/models/CaseModel.php
@@ -10,7 +10,7 @@ class CaseModel extends Model
 {
     use \October\Rain\Database\Traits\Validation;
     use \Demo\Core\Classes\Traits\ModelHelper;
-    use \Demo\Core\Classes\Traits\WorkflowEntity;
+    use \Demo\Workflow\Classes\Traits\WorkflowEntity;
 
     /**
      * @var string The database table used by the model.
