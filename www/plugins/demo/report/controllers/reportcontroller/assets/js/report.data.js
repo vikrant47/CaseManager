754fb776a6201db3faf8ea84eef306739d8ee6f1
vikrant singh <vikrant.singh1990@yahoo.in>
2019-12-01 21:18:07 +0530

    Added dashobard

--- www/plugins/demo/report/controllers/reportcontroller/assets/js/report.data.js
+++ www/plugins/demo/report/controllers/reportcontroller/assets/js/report.data.js
@@ -5,9 +5,21 @@ var Report = function (id) {
     this.id = id;
 };
 Object.assign(Report.prototype, {
+    getCanvas: function () {
+        var $elem = $('#report-container-' + this.id);
+        var $canvas = $elem.find('canvas');
+        if ($canvas.length === 0) {
+            $canvas = $('<canvas style="width: 100%;height: 100%;"/>').appendTo($elem);
+        }
+        return $canvas.get(0);
+    },
+    getContainer: function () {
+        return $('#report-container-' + this.id).get(0);
+    },
     loadData: function (callabck) {
         var _this = this;
         $.request('onData', {
+            url:'/backend/demo/report/reportcontroller/render-report/'+this.slug,
             data: {id: this.id},
             success: function (data) {
                 var data = JSON.parse(data.result);
---
+++ www/plugins/demo/report/models/Dashboard.php
@@ -0,0 +1,36 @@
+<?php namespace Demo\Report\Models;
+
+use Demo\Core\Models\JavascriptLibrary;
+use Demo\Core\Models\PluginVersions;
+use Model;
+
+/**
+ * Model
+ */
+class Dashboard extends Model
+{
+    use \October\Rain\Database\Traits\Validation;
+
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'demo_report_dashboards';
+
+    /**
+     * @var array Validation rules
+     */
+    public $rules = [
+    ];
+
+
+    public $attachAuditedBy = true;
+
+
+    public $belongsTo = [
+        'plugin' => [PluginVersions::class, 'key' => 'plugin_id'],
+        'report' => [Report::class]
+    ];
+
+    public $jsonable = ['reports_config'];
+}
