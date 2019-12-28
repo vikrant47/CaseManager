b0cc0acdfea687974e56d31f0b9ba474aa77b4f7
vikrant singh <vikrant.singh1990@yahoo.in>
2019-12-28 15:10:31 +0530

    Minor fixes

--- www/plugins/demo/workflow/seeds/builder_table_seed_demo_workflow_queue_routing_rules.php
+++ www/plugins/demo/workflow/seeds/builder_table_seed_demo_workflow_queue_routing_rules.php
@@ -15,7 +15,7 @@ class BuilderTableSeedDemoQueueRoutingRules extends Seeder
             "updated_at" => "2019-11-24 03:58:03",
             "created_by_id" => 1,
             "updated_by_id" => 1,
-            "script" => "if(empty(\$item)){\r\n    throw new \$context->exception->ApplicationException('No item left to assign');\r\n}\r\n\$item->assigned_to = \$context->currentUser;\r\n\$item->save();\r\nreturn \$context->currentUser;",
+            "script" => "\$item = \$context->item; \r\n if(empty(\$item)){\r\n    throw new \$context->exception->ApplicationException('No item left to assign');\r\n}\r\n\$item->assigned_to = \$context->currentUser;\r\n\$item->save();\r\nreturn \$context->currentUser;",
             "name" => "Route to current User",
             "description" => "Route to current User",
             "plugin_id" => 1
