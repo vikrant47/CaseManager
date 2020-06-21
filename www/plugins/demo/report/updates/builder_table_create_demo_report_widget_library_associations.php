ee201c94dde9d3c29a6029a8f2333548a7a6b2e4
vikrant singh <vikrant.singh1990@yahoo.in>
2020-06-21 09:54:08 +0530

    Added some libs

---
+++ www/plugins/demo/report/updates/builder_table_create_demo_report_widget_library_associations.php
@@ -0,0 +1,28 @@
+<?php namespace Demo\Report\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateDemoReportWidgetLibraryAssociations extends Migration
+{
+    public function up()
+    {
+        Schema::create('demo_report_widget_library_associations', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->uuid('id')->primary();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->integer('created_by_id');
+            $table->integer('updated_by_id');
+            $table->integer('plugin_id');
+            $table->uuid('widget_id');
+            $table->uuid('library_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('demo_report_widget_library_associations');
+    }
+}
