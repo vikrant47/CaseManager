98fb27eaa0852356b00d907174013126f2c0f42f
Suraj Baliyan <suraj.baliyan4@gmail.com>
2020-06-27 19:46:57 +0530

    Duration Widget css and js delete

--- www/plugins/demo/core/formwidgets/DurationWidget.php
+++ www/plugins/demo/core/formwidgets/DurationWidget.php
@@ -12,7 +12,9 @@ use October\Rain\Support\Facades\Form;
 class DurationWidget extends FormWidgetBase
 {
     public $selectedValue="days";
-    public $DeferenceInDays="";
+    public $DeferenceInDays;
+    public $taskEndTime;
+
     public function widgetDetails(){
         return[
             'name'=>'Duration',
@@ -29,47 +31,61 @@ class DurationWidget extends FormWidgetBase
      */
     public function prepareVars()
     {
-        Carbon::setLocale('in');
-        $created = Carbon::createFromTimestamp($this->getLoadValue())->toDateTimeString();
-        $todayDate = Carbon::today();
-        if($todayDate->diffInMonths($created)<=0){
-            if($todayDate->diffInWeeks($created)>0){
-                $this->DeferenceInDays = $todayDate->diffInWeeks($created);
-                $this->selectedValue= 'weeks';
-            }else if($todayDate->diffInWeeks($created)<=0 && $todayDate->diffInDays($created)>0){
-                $this->DeferenceInDays = $todayDate->diffInDays($created);
-                $this->selectedValue= 'days';
-            }else if($todayDate->diffInDays($created)<=0 && $todayDate->diffInHours($created)>0){
-                $this->DeferenceInDays = $todayDate->diffInHours($created);
-                $this->selectedValue= 'hours';
-            }else{
-                $this->DeferenceInDays = $todayDate->diffInSeconds($created);
-                $this->selectedValue= 'sec';
-            }
-        }else if($todayDate->diffInMonths($created)>0 && $todayDate->diffInMonths($created)<=11){
-            $this->DeferenceInDays = $todayDate->diffInMonths($created);
-            $this->selectedValue= 'months';
-        }else if($todayDate->diffInMonths($created)>11){
-            $this->DeferenceInDays = $todayDate->diffInYears($created);
-            $this->selectedValue= 'years';
-        }
-
         $this->vars['placeholder'] = $this->placeholder;
         $this->vars['field'] = $this->formField;
+        $this->populateSelectedType();
+    }
+    public function populateSelectedType(){
+        $updated = Carbon::createFromTimestamp($this->getLoadValue());
+        $now = Carbon::now();
+        if ($updated->diff($now)->m < 1) {
+            $lastOnline = $updated->lte($now);
+            $this->DeferenceInDays = $updated->lte($now)?'':$updated->diffInSeconds($now);
+            $this->selectedValue= 'sec';
+        } else if ($updated->diff($now)->h < 1) {
+            $lastOnline = $updated->diffInMinutes($now);
+            $this->DeferenceInDays = $lastOnline;
+            $this->selectedValue= 'min';
+        } else if ($updated->diff($now)->d < 1) {
+            $lastOnline = $updated->diffInHours($now);
+            $this->DeferenceInDays = $lastOnline;//Carbon::createFromTimestamp($this->getLoadValue())->hour-Carbon::now()->hour;
+            $this->selectedValue= 'hours';
+        } else if ($updated->diffInWeeks($now)<0) {
+            $lastOnline = $updated->diffInDays($now);
+            $this->DeferenceInDays = $lastOnline;//Carbon::createFromTimestamp($this->getLoadValue())->hour-Carbon::now()->hour;
+            $this->selectedValue= 'days';
+        } else if ($updated->diffInWeeks($now)>0 && $updated->diffInMonths($now) < 12) {
+            $lastOnline = $updated->diffInWeeks(Carbon::today());
+            $this->DeferenceInDays = $lastOnline;
+            $this->selectedValue= 'weeks';
+        } else if ($updated->diff($now)->y < 1) {
+            $lastOnline = $updated->diffInMonths(Carbon::today());
+            $this->DeferenceInDays = $lastOnline;
+            $this->selectedValue= 'months';
+        } else {
+            $lastOnline = $updated->diffInYears(Carbon::today());
+            $this->DeferenceInDays = $lastOnline;
+            $this->selectedValue= 'years';
+        }
+        dump($lastOnline);
+        dump(Carbon::now()->diff(Carbon::createFromTimestamp($this->getLoadValue()))->format('%H:%I:%S'));
         $this->selectedValue= $this->selectedValue;
         $this->formField->value =$this->DeferenceInDays;
-
+        $this->taskEndTime=$updated;
     }
-
     /**
      * @inheritDoc
      */
     public function getSaveValue($value)
     {
-        Carbon::setLocale('in');
+        $selectedType = Input::get('_tatSelectedType');
+        $endTime=$this->setEndTime($selectedType,$value);
+        return $endTime;
+    }
+
+    public function setEndTime($selectedType,$value){
         $startDay = Carbon::now();
         $endTime="";
-        $selectedType = Input::get('_tatSelectedType');
         if($selectedType=="days"){
             $endTime=$startDay->addDay($value)->timestamp;
         }else if($selectedType=="weeks"){
@@ -80,6 +96,8 @@ class DurationWidget extends FormWidgetBase
             $endTime=$startDay->addYear($value)->timestamp;
         }else if($selectedType=="hours"){
             $endTime=$startDay->addHours($value)->timestamp;
+        }else if($selectedType=="min"){
+            $endTime=$startDay->addMinutes($value)->timestamp;
         }else{
             $endTime=$startDay->addSeconds($value)->timestamp;
         }
