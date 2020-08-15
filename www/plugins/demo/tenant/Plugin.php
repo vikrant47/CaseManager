619b078adc7b0dfbe9fdca4fc27cd67447ae2290
vikrant singh <vikrant.singh1990@yahoo.in>
2020-08-15 22:04:34 +0530

    sql script updated

---
+++ www/plugins/demo/tenant/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Demo\Tenant;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
